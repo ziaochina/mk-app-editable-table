e0e7524cb8f44c6602fa2db08cd819d3508dc2f3
ziaochina <ziaochina@gmail.com>
2017-10-20 16:35:36 +0800

    zlj

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as data from './data'
 
 export default {
 	name: "mk-app-editable-table",
-	version: "1.0.5",
+	version: "1.0.7",
 	description: "mk-app-editable-table",
 	meta: data.getMeta(),
 	components: [],
