748bb9b19772020669157eed8ff528e2b629037a
ziaochina <ziaochina@gmail.com>
2017-10-20 16:27:37 +0800

    zlj

--- reducer.js
+++ reducer.js
@@ -20,28 +20,6 @@ class reducer {
         state = this.metaReducer.sf(state, 'data.list', fromJS(response.list))
         return this.metaReducer.sf(state, 'data.other.focusCellInfo', undefined)
     }
-
-    addEmptyRow = (state, rowIndex) => {
-        var list = this.metaReducer.gf(state, 'data.list')
-        list = list.insert(rowIndex, Map({
-            id: list.size
-        }))
-
-        return this.metaReducer.sf(state, 'data.list', list)
-    }
-
-    delrow = (state, id) => {
-        var list = this.metaReducer.gf(state, 'data.list')
-        const index = list.findIndex(o => {
-            return o.get('id') == id
-        })
-
-        if (index == -1)
-            return state
-
-        list = list.remove(index)
-        return this.metaReducer.sf(state, 'data.list', list)
-    }
 }
 
 export default function creator(option) {
